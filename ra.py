d7ee016bc12cc9ce270519c2361e4ed6bc52f965
Thomas Hsu <th30845@gmail.com>
2019-09-20 16:47:34 +0800

    per 1000 print len data

--- ra.py
+++ ra.py
@@ -1,11 +1,16 @@
 #
-data = []
+data = []   #empty list
+count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
-print(len(data))
+		count += 1    #equal to count = count + 1
+		if count % 1000 == 0:  # % = 餘數,每1000次才印一次
+			print(len(data)) #print 1000000
+print(len(data))  #print data length
 
-print(data[0])
+print(data[0])    #首段
 print('-----------------')
-print(data[1])
+print('\n')
+print(data[999999])    #第二段
 print('-----------------')
