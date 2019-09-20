6acdb1085c6c01a250aaf6931dfed91a69dba2f5
Thomas Hsu <th30845@gmail.com>
2019-09-20 17:03:41 +0800

    Avg Reviews length cal

--- ra.py
+++ ra.py
@@ -7,7 +7,7 @@ with open('reviews.txt', 'r') as f:
 		count += 1    #equal to count = count + 1
 		if count % 1000 == 0:  # % = 餘數,每1000次才印一次
 			print(len(data)) #print 1000000
-print('檔案讀取完了總共有', len(data), '筆資料')  #print data length
+print('There is total', len(data), 'piece of data')  #print data length
 
 #計算平均長度
 
@@ -16,5 +16,4 @@ for d in data:   # d is string
 	sum_len = sum_len + len(d)    #equal sum_len = sum_len + len(d)
 	#print(sum_len)
 
-# if len(data) == 1000000
 print('Avg Reviews length is: ', sum_len/len(data))
