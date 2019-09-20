afdf127cfb1d57bfae43728950f80756d3285d2c
Thomas Hsu <th30845@gmail.com>
2019-09-20 17:23:54 +0800

    Add filter

--- ra.py
+++ ra.py
@@ -17,3 +17,15 @@ for d in data:   # d is string
 	#print(sum_len)
 
 print('Avg Reviews length is: ', sum_len/len(data))
+
+#篩選
+
+new = []
+for d in data: #d is string , data = 1million piece of data list
+	if len(d) < 100:  #len(d) = length of d < 100
+		new.append(d)
+		#print('There are :', len(new), 'piece of data string length below 100')
+		#↑if this situation then print in for loop
+print('There are :', len(new), 'piece of data string length below 100')
+#↑ print after for loop end
+print(new[0])
